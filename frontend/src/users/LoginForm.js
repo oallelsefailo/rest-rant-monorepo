42a3380e3e3694e53f7d53a4883a51b0c57f3f0b
Brandon <oallelsefailo@gmail.com>
2023-10-25 18:24:20 -0700

    Assignemnt done

--- frontend/src/users/LoginForm.js
+++ frontend/src/users/LoginForm.js
@@ -14,24 +14,25 @@ function LoginForm() {
 
   const [errorMessage, setErrorMessage] = useState(null);
 
-  async function handleSubmit(e) {
-    const response = await fetch(`http://localhost:5000/authentication/`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(credentials),
-    });
+async function handleSubmit(e) {
+  const response = await fetch(`http://localhost:5000/authentication/`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(credentials),
+  });
 
-    const data = await response.json();
+  const data = await response.json();
 
-    if (response.status === 200) {
-      setCurrentUser(data.user);
-      history.push(`/`);
-    } else {
-      setErrorMessage(data.message);
-    }
+  if (response.status === 200) {
+    setCurrentUser(data.user);
+    localStorage.setItem("token", data.token);
+    history.push(`/`);
+  } else {
+    setErrorMessage(data.message);
   }
+}
   return (
     <main>
       <h1>Login</h1>
