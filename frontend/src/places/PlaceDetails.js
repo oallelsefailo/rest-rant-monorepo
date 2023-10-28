165fd126ead311c5858dfe50c32923c035a51228
Brandon <oallelsefailo@gmail.com>
2023-10-28 12:43:23 -0700

    Assignment done

--- frontend/src/places/PlaceDetails.js
+++ frontend/src/places/PlaceDetails.js
@@ -107,6 +107,20 @@ function PlaceDetails() {
 		})
 	}
 
+	let placeActions = null;
+
+  if (currentUser?.role === "admin") {
+    placeActions = (
+      <>
+        <a className="btn btn-warning" onClick={editPlace}>
+          Edit
+        </a>
+        <button type="submit" className="btn btn-danger" onClick={deletePlace}>
+          Delete
+        </button>
+      </>
+    );
+  }
 
 	return (
 		<main>
@@ -134,12 +148,7 @@ function PlaceDetails() {
 						Serving {place.cuisines}.
 					</h4>
 					<br />
-					<a className="btn btn-warning" onClick={editPlace}>
-						Edit
-					</a>{` `}
-					<button type="submit" className="btn btn-danger" onClick={deletePlace}>
-						Delete
-					</button>
+					{placeActions}
 				</div>
 			</div>
 			<hr />
